0c5e829a930fb2c9ead9c7aabf60cba6aaaee2e8
Blake Dietz <blakedietz@gmail.com>
2025-03-23 16:09:27 -0600

    build: enable code splitting

--- config/config.exs
+++ config/config.exs
@@ -35,8 +35,7 @@ config :app, App.Mailer, adapter: Swoosh.Adapters.Local
 config :esbuild,
   version: "0.25.1",
   app: [
-    args:
-      ~w(js/app.jsx --bundle --target=es2020 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+    args: ~w(js/app.jsx --bundle --chunk-names=chunks/[name]-[hash] --splitting --format=esm --target=es2020 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
