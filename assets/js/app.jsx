b795fe17620264bd8b2fcd9da97957d329d5f2a0
Blake Dietz <blakedietz@gmail.com>
2025-03-23 17:10:01 -0600

    fix: without this logout is broken
    I haven't played with forms yet, so I'm not sure how this is going to
    behave once I start adding some.

--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -3,12 +3,13 @@ import axios from "axios";
 
 import { createInertiaApp } from "@inertiajs/react";
 import { createRoot } from "react-dom/client";
+import "phoenix_html";
 
 axios.defaults.xsrfHeaderName = "x-csrf-token";
 
 createInertiaApp({
   resolve: async (name) => {
-    return await import(`./pages/${name}.jsx`);
+    return await import(`./pages/${name}.tsx`);
   },
   setup({ App, el, props }) {
     createRoot(el).render(<App {...props} />);
